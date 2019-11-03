6adab7c2eac8a563f345574b2d3f7253af6f2136
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-03 15:36:59 +0000

    refactored code

--- directory.rb
+++ directory.rb
@@ -1,56 +1,59 @@
 # get user input for student names
 def input_students
-  puts "Please enther the names of the students"
+  puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: :november}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     
     name = gets.chomp
   end
-  students
+  @students
 end
 # print out the students
 def print_header
   puts "The students of Villains Academy"
   puts "-" * 10
 end
-def print(students)
-  students.each_with_index { |student, index| puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" }
+def print_students_list
+  @students.each_with_index { |student, index| puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" }
 end
-def print_footer(students)
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student"
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
   else
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
 
 def interactive_menu
-  students = []
+  @students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    
+    print_menu
     selection = gets.chomp
-
     case selection 
     when "1"
-      students = input_students
+      @students = input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
