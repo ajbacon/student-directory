16c352913b0d18570c321f50d6226cf6bdc8564c
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-04 20:40:13 +0000

    added load functionality, tidied up minor errors

--- directory.rb
+++ directory.rb
@@ -36,14 +36,17 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save Students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
+  puts
 end
 
 def show_students
   print_header
   print_students_list
   print_footer
+  puts
 end
 
 def interactive_menu
@@ -57,6 +60,8 @@ def interactive_menu
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -66,7 +71,7 @@ def interactive_menu
 end
 
 def save_students
-  file = File.open("students.csv", "a")
+  file = File.open("students.csv", "w")
   @students.each do |student| 
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -74,6 +79,18 @@ def save_students
   end
   file.close
   puts "Students saved to 'students.csv'"
+  puts
+end
+
+def load_students
+  file = File.open("students.csv")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  puts "#{@students.count} Students loaded from 'students.csv'"
+  puts
 end
 
 interactive_menu
