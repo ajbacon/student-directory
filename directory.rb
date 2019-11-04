0001248ca7f6334994ec1b30a8615cbf24c9f6d9
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-04 20:07:11 +0000

    added save students to file functionality

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+@students = []
 # get user input for student names
 def input_students
   puts "Please enter the names of the students"
@@ -35,6 +36,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save Students"
   puts "9. Exit"
 end
 
@@ -45,7 +47,6 @@ def show_students
 end
 
 def interactive_menu
-  @students = []
   loop do
     print_menu
     selection = gets.chomp
@@ -54,6 +55,8 @@ def interactive_menu
       @students = input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -61,5 +64,18 @@ def interactive_menu
     end
   end
 end
+
+def save_students
+  file = File.open("students.csv", "a")
+  @students.each do |student| 
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+  puts "Students saved to 'students.csv'"
+end
+
+
 
 interactive_menu
